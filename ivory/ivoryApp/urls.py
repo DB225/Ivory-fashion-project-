c97524b37a4ded5af17fe95eec4a383d8775112c
marcus110379 <marcusjmadden@gmail.com>
2019-05-29 09:08:54 -0500

    endpoints done

--- ivory/ivoryApp/urls.py
+++ ivory/ivoryApp/urls.py
@@ -2,5 +2,34 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Main page
     path('', views.index, name='index'),
+
+    # Men routes
+    path('men/', views.men, name='men'),
+    path('tshirtmen', views.tshirtmen, name='tshirtmen'),
+    path('shirtmen', views.shirtmen, name='shirtmen'),
+    path('pantsmen/', views.pantsmen, name='pantsmen'),
+    path('shoesmen/', views.shoesmen, name='shoesmen'),
+
+    # Women routes
+    path('women/', views.women, name='women'),
+    path('dress/', views.dress, name='dress'),
+    path('bagjewel/', views.bagjewel, name='bagjewel'),
+    path('shoeswomen/', views.shoeswomen, name='shoeswomen'),
+
+    # Boys routes
+    path('boys/', views.boys, name='boys'),
+    # path('boys/', views.boys, name='boys'),
+    # path('boys/', views.boys, name='boys'),
+    # path('boys/', views.boys, name='boys'),
+
+    # Girls routes
+    path('girls/', views.girls, name='girls'),
+    # path('girls/', views.girls, name='girls'),
+    # path('girls/', views.girls, name='girls'),
+    # path('girls/', views.girls, name='girls'),
+
+    # Cart
+    path('shoppingCart/', views.shoppingCart, name='shoppingCart'),
 ]
--- ivory/ivoryApp/views.py
+++ ivory/ivoryApp/views.py
@@ -1,7 +1,55 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def index(request):
     return render(request, 'ivoryApp/index.html')
+
+
+# MEN FUNCTIONS
+def men(request):
+    return render(request, 'ivoryApp/men.html')
+
+
+def tshirtmen(request):
+    return render(request, 'ivoryApp/tShirtMen.html')
+
+
+def shirtmen(request):
+    return render(request, 'ivoryApp/shirtMen.html')
+
+
+def pantsmen(request):
+    return render(request, 'ivoryApp/pantsMen.html')
+
+
+def shoesmen(request):
+    return render(request, 'ivoryApp/shoesMen.html')
+
+#################################################################################
+# WOMEN FUNCTIONS
+def women(request):
+    return render(request, 'ivoryApp/women.html')
+
+
+def dress(request):
+    return render(request, 'ivoryApp/dress.html')
+
+
+def bagjewel(request):
+    return render(request, 'ivoryApp/bagjewelry.html')
+
+
+def shoeswomen(request):
+    return render(request, 'ivoryApp/shoesWomen.html')
+#################################################################################
+def boys(request):
+    return render(request, 'ivoryApp/boys.html')
+
+
+def girls(request):
+    return render(request, 'ivoryApp/girls.html')
+
+
+def shoppingCart(request):
+    return render(request, 'ivoryApp/shoppingCart.html')
